374ee777f37c3690e01794aa54a6ab57b150658f
kdduha <just.andrew.kd@gmail.com>
2024-01-17 18:32:38 +0300

    implementation of the forecaster model from the article

--- torchcnnbuilder/models.py
+++ torchcnnbuilder/models.py
@@ -8,18 +8,40 @@ import torch.nn as nn
 # CNN Forecaster pattern class
 # ------------------------------------
 class ForecasterBase(nn.Module):
-    def __int__(self,
-                input_size: Sequence[int],
-                n_layers: int,
-                in_channels: int,
-                out_channels: int,
-                n_transpose_layers: Optional[int] = None,
-                convolve_params: Optional[dict] = None,
-                transpose_convolve_params: Optional[dict] = None,
-                activation_function: nn.Module = nn.ReLU(inplace=True),
-                finish_activation_function: Union[str, Optional[nn.Module]] = None,
-                normalization: Optional[str] = None):
+    """
+      The template class of the time series prediction CNN-architecture. The source of the original `article
+    <https://github.com/ITMO-NSS-team/ice-concentration-prediction-paper?ysclid=lrhxbvsk8s328492826>`_.
 
+      Attributes:
+          convolve (nn.Sequential): convolutional sequence - encoder part
+          transpose (nn.Sequential): transpose convolutional sequence - decoder part
+       """
+    def __init__(self,
+                 input_size: Sequence[int],
+                 n_layers: int,
+                 in_channels: int,
+                 out_channels: int,
+                 n_transpose_layers: Optional[int] = None,
+                 convolve_params: Optional[dict] = None,
+                 transpose_convolve_params: Optional[dict] = None,
+                 activation_function: nn.Module = nn.ReLU(inplace=True),
+                 finish_activation_function: Union[str, Optional[nn.Module]] = None,
+                 normalization: Optional[str] = None) -> None:
+        """
+        The constructor for ForecasterBase
+
+        :param input_size: input size of the input tensor
+        :param n_layers: number of the convolution layers in the encoder part
+        :param in_channels: number of channels in the first input tensor (prehistory size)
+        :param out_channels: number of channels in the last output tensor (forecasting size)
+        :param n_transpose_layers: number of the transpose convolution layers in the encoder part. Default: None (same as n_layers)
+        :param convolve_params: parameters of convolutional layers (by default same as in torch). Default: None
+        :param transpose_convolve_params: parameters of transpose convolutional layers (by default same as in torch). Default: None
+        :param activation_function: activation function. Default: nn.ReLU(inplace=True
+        :param finish_activation_function: last activation function, can be same as activation_function (str 'same'). Default: None
+        :param normalization: choice of normalization between str 'dropout' and 'batchnorm'. Default: None
+        """
+        super(ForecasterBase, self).__init__()
         builder = EncoderBuilder(input_size=input_size,
                                  activation_function=activation_function,
                                  finish_activation_function=finish_activation_function)
@@ -47,6 +69,12 @@ class ForecasterBase(nn.Module):
                                                                    ascending=True)
 
     def forward(self, x):
+        """
+        Forward pass of the model
+
+        :param x: tensor before forward pass
+        :return: tensor after forward pass
+        """
         x = self.convolve(x)
         x = self.transpose(x)
         return x
