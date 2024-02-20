6259391ea55e7892b7d2d0a7dd5aebb3c8666833
kdduha <just.andrew.kd@gmail.com>
2024-02-20 18:14:04 +0300

    adding `n_layers` to conv formulas

--- torchcnnbuilder/builder.py
+++ torchcnnbuilder/builder.py
@@ -29,18 +29,20 @@ def _triple_params(param: int) -> Tuple[int, int, int]:
 # calculation of convolution functions
 # ------------------------------------
 def conv1d_out(input_size: Union[Tuple[int], int],
-               kernel_size: Union[Tuple[int], int],
+               kernel_size: Union[Tuple[int], int] = 3,
                stride: Union[Tuple[int], int] = 1,
                padding: Union[Tuple[int], int] = 0,
-               dilation: Union[Tuple[int], int] = 1) -> Tuple[int]:
+               dilation: Union[Tuple[int], int] = 1,
+               n_layers: int = 1) -> Tuple[int]:
     """
     Calculating the size of the tensor after nn.Conv1d
 
     :param input_size: size of the input tensor/vector [h]
-    :param kernel_size: size of the convolution kernel
+    :param kernel_size: size of the convolution kernel. Default: 3
     :param stride: stride of the convolution. Default: 1
     :param padding: padding added to all four sides of the input. Default: 0
     :param dilation: spacing between kernel elements. Default: 1
+    :param n_layers: number of conv layers
     :return int: size of the output tensor/vector [h] as a tuple
     """
     input_size = (input_size,) if isinstance(input_size, int) else input_size
@@ -49,23 +51,27 @@ def conv1d_out(input_size: Union[Tuple[int], int],
     kernel_size = (kernel_size,) if isinstance(kernel_size, int) else kernel_size
     stride = (stride,) if isinstance(stride, int) else stride
 
-    h_out = (input_size[0] + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) / stride[0] + 1
-    return (int(h_out),)
+    for _ in range(n_layers):
+        h_out = (input_size[0] + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) / stride[0] + 1
+        input_size = (int(h_out),)
+    return input_size
 
 
 def conv2d_out(input_size: Union[Sequence[int], int],
-               kernel_size: Union[Sequence[int], int],
+               kernel_size: Union[Sequence[int], int] = 3,
                stride: Union[Sequence[int], int] = 1,
                padding: Union[Sequence[int], int] = 0,
-               dilation: Union[Sequence[int], int] = 1) -> Tuple[int, int]:
+               dilation: Union[Sequence[int], int] = 1,
+               n_layers: int = 1) -> Tuple[int, int]:
     """
     Calculating the size of the tensor after nn.Conv2d
 
     :param input_size: size of the input tensor [h, w]
-    :param kernel_size: size of the convolution kernel
+    :param kernel_size: size of the convolution kernel. Default: 3
     :param stride: stride of the convolution. Default: 1
     :param padding: padding added to all four sides of the input. Default: 0
     :param dilation: spacing between kernel elements. Default: 1
+    :param n_layers: number of conv layers
     :return Tuple[int, int]: size of the output tensor [h, w]
     """
     input_size = _double_params(input_size) if isinstance(input_size, int) else input_size
@@ -74,24 +80,28 @@ def conv2d_out(input_size: Union[Sequence[int], int],
     kernel_size = _double_params(kernel_size) if isinstance(kernel_size, int) else kernel_size
     stride = _double_params(stride) if isinstance(stride, int) else stride
 
-    h_out = conv1d_out(input_size[0], kernel_size[0], stride[0], padding[0], dilation[0])[0]
-    w_out = conv1d_out(input_size[1], kernel_size[1], stride[1], padding[1], dilation[1])[0]
-    return int(h_out), int(w_out)
+    for _ in range(n_layers):
+        h_out = conv1d_out(input_size[0], kernel_size[0], stride[0], padding[0], dilation[0])[0]
+        w_out = conv1d_out(input_size[1], kernel_size[1], stride[1], padding[1], dilation[1])[0]
+        input_size = (int(h_out), int(w_out))
+    return input_size
 
 
 def conv3d_out(input_size: Union[Sequence[int], int],
-               kernel_size: Union[Sequence[int], int],
+               kernel_size: Union[Sequence[int], int] = 3,
                stride: Union[Sequence[int], int] = 1,
                padding: Union[Sequence[int], int] = 0,
-               dilation: Union[Sequence[int], int] = 1) -> Tuple[int, int, int]:
+               dilation: Union[Sequence[int], int] = 1,
+               n_layers: int = 1) -> Tuple[int, int, int]:
     """
     Calculating the size of the tensor after nn.Conv3d
 
     :param input_size: size of the input tensor [d, h, w]
-    :param kernel_size: size of the convolution kernel
+    :param kernel_size: size of the convolution kernel. Default: 3
     :param stride: stride of the convolution. Default: 1
     :param padding: padding added to all four sides of the input. Default: 0
     :param dilation: spacing between kernel elements. Default: 1
+    :param n_layers: number of conv layers
     :return Tuple[int, int, int]: size of the output tensor [d, h, w]
     """
     input_size = _triple_params(input_size) if isinstance(input_size, int) else input_size
@@ -100,30 +110,34 @@ def conv3d_out(input_size: Union[Sequence[int], int],
     kernel_size = _triple_params(kernel_size) if isinstance(kernel_size, int) else kernel_size
     stride = _triple_params(stride) if isinstance(stride, int) else stride
 
-    d_out = conv1d_out(input_size[0], kernel_size[0], stride[0], padding[0], dilation[0])[0]
-    h_out = conv1d_out(input_size[1], kernel_size[1], stride[1], padding[1], dilation[1])[0]
-    w_out = conv1d_out(input_size[2], kernel_size[2], stride[2], padding[2], dilation[2])[0]
-    return int(d_out), int(h_out), int(w_out)
+    for _ in range(n_layers):
+        d_out = conv1d_out(input_size[0], kernel_size[0], stride[0], padding[0], dilation[0])[0]
+        h_out = conv1d_out(input_size[1], kernel_size[1], stride[1], padding[1], dilation[1])[0]
+        w_out = conv1d_out(input_size[2], kernel_size[2], stride[2], padding[2], dilation[2])[0]
+        input_size = (int(d_out), int(h_out), int(w_out))
+    return input_size
 
 
 # ------------------------------------
 # calculation of transposed convolution functions
 # ------------------------------------
 def conv_transpose1d_out(input_size: Union[Tuple[int], int],
-                         kernel_size: Union[Tuple[int], int],
+                         kernel_size: Union[Tuple[int], int] = 3,
                          stride: Union[Tuple[int], int] = 1,
                          padding: Union[Tuple[int], int] = 0,
                          output_padding: Union[Tuple[int], int] = 0,
-                         dilation: Union[Tuple[int], int] = 1) -> Tuple[int]:
+                         dilation: Union[Tuple[int], int] = 1,
+                         n_layers: int = 1) -> Tuple[int]:
     """
     Calculating the size of the tensor after nn.ConvTranspose1d
 
     :param input_size: size of the input tensor/vector [h]
-    :param kernel_size: size of the transposed convolution kernel
+    :param kernel_size: size of the transposed convolution kernel. Default: 3
     :param stride: stride of the transposed convolution. Default: 1
     :param padding: padding added to all four sides of the input. Default: 0
     :param output_padding: controls the additional size added to one side of the output shape. Default: 0
     :param dilation: spacing between kernel elements. Default: 1
+    :param n_layers: number of conv layers
     :return int: size of the output tensor/vector [h] as a tuple
     """
     input_size = (input_size,) if isinstance(input_size, int) else input_size
@@ -133,26 +147,29 @@ def conv_transpose1d_out(input_size: Union[Tuple[int], int],
     kernel_size = (kernel_size,) if isinstance(kernel_size, int) else kernel_size
     stride = (stride,) if isinstance(stride, int) else stride
 
-    h_out = (input_size[0] - 1) * stride[0] - 2 * padding[0] + dilation[0] * (kernel_size[0] - 1) + output_padding[
-        0] + 1
-    return (int(h_out),)
+    for _ in range(n_layers):
+        h_out = (input_size[0] - 1) * stride[0] - 2 * padding[0] + dilation[0] * (kernel_size[0] - 1) + output_padding[0] + 1
+        input_size = (int(h_out),)
+    return input_size
 
 
 def conv_transpose2d_out(input_size: Union[Sequence[int], int],
-                         kernel_size: Union[Sequence[int], int],
+                         kernel_size: Union[Sequence[int], int] = 3,
                          stride: Union[Sequence[int], int] = 1,
                          padding: Union[Sequence[int], int] = 0,
                          output_padding: Union[Sequence[int], int] = 0,
-                         dilation: Union[Sequence[int], int] = 1) -> Tuple[int, int]:
+                         dilation: Union[Sequence[int], int] = 1,
+                         n_layers: int = 1) -> Tuple[int, int]:
     """
     Calculating the size of the tensor after nn.ConvTranspose2d
 
     :param input_size: size of the input tensor [h, w]
-    :param kernel_size: size of the transposed convolution kernel
+    :param kernel_size: size of the transposed convolution kernel. Default: 3
     :param stride: stride of the transposed convolution. Default: 1
     :param padding: padding added to all four sides of the input. Default: 0
     :param output_padding: controls the additional size added to one side of the output shape. Default: 0
     :param dilation: spacing between kernel elements. Default: 1
+    :param n_layers: number of conv layers
     :return Tuple[int, int]: size of the output tensor [h, w]
     """
     input_size = _double_params(input_size) if isinstance(input_size, int) else input_size
@@ -162,28 +179,30 @@ def conv_transpose2d_out(input_size: Union[Sequence[int], int],
     kernel_size = _double_params(kernel_size) if isinstance(kernel_size, int) else kernel_size
     stride = _double_params(stride) if isinstance(stride, int) else stride
 
-    h_out = conv_transpose1d_out(input_size[0], kernel_size[0], stride[0], padding[0], output_padding[0], dilation[0])[
-        0]
-    w_out = conv_transpose1d_out(input_size[1], kernel_size[1], stride[1], padding[1], output_padding[1], dilation[1])[
-        0]
-    return int(h_out), int(w_out)
+    for _ in range(n_layers):
+        h_out = conv_transpose1d_out(input_size[0], kernel_size[0], stride[0], padding[0], output_padding[0], dilation[0])[0]
+        w_out = conv_transpose1d_out(input_size[1], kernel_size[1], stride[1], padding[1], output_padding[1], dilation[1])[0]
+        input_size = (int(h_out), int(w_out))
+    return input_size
 
 
 def conv_transpose3d_out(input_size: Union[Sequence[int], int],
-                         kernel_size: Union[Sequence[int], int],
+                         kernel_size: Union[Sequence[int], int] = 3,
                          stride: Union[Sequence[int], int] = 1,
                          padding: Union[Sequence[int], int] = 0,
                          output_padding: Union[Sequence[int], int] = 0,
-                         dilation: Union[Sequence[int], int] = 1) -> Tuple[int, int, int]:
+                         dilation: Union[Sequence[int], int] = 1,
+                         n_layers: int = 1) -> Tuple[int, int, int]:
     """
     Calculating the size of the tensor after nn.ConvTranspose3d
 
     :param input_size: size of the input tensor [d, h, w]
-    :param kernel_size: size of the transposed convolution kernel
+    :param kernel_size: size of the transposed convolution kernel. Default: 3
     :param stride: stride of the transposed convolution. Default: 1
     :param padding: padding added to all four sides of the input. Default: 0
     :param output_padding: controls the additional size added to one side of the output shape. Default: 0
     :param dilation: spacing between kernel elements. Default: 1
+    :param n_layers: number of conv layers
     :return Tuple[int, int]: size of the output tensor [d, h, w]
     """
     input_size = _triple_params(input_size) if isinstance(input_size, int) else input_size
@@ -193,13 +212,12 @@ def conv_transpose3d_out(input_size: Union[Sequence[int], int],
     kernel_size = _triple_params(kernel_size) if isinstance(kernel_size, int) else kernel_size
     stride = _triple_params(stride) if isinstance(stride, int) else stride
 
-    d_out = conv_transpose1d_out(input_size[0], kernel_size[0], stride[0], padding[0], output_padding[0], dilation[0])[
-        0]
-    h_out = conv_transpose1d_out(input_size[1], kernel_size[1], stride[1], padding[1], output_padding[1], dilation[1])[
-        0]
-    w_out = conv_transpose1d_out(input_size[2], kernel_size[2], stride[2], padding[2], output_padding[2], dilation[1])[
-        0]
-    return int(d_out), int(h_out), int(w_out)
+    for _ in range(n_layers):
+        d_out = conv_transpose1d_out(input_size[0], kernel_size[0], stride[0], padding[0], output_padding[0], dilation[0])[0]
+        h_out = conv_transpose1d_out(input_size[1], kernel_size[1], stride[1], padding[1], output_padding[1], dilation[1])[0]
+        w_out = conv_transpose1d_out(input_size[2], kernel_size[2], stride[2], padding[2], output_padding[2], dilation[1])[0]
+        input_size = (int(d_out), int(h_out), int(w_out))
+    return input_size
 
 
 # ------------------------------------
