c3e349dea9f31d6ba82ecf272a5e2aa6584abb47
kdduha <just.andrew.kd@gmail.com>
2024-11-04 14:19:54 +0300

    refactor doc dir order

--- build_and_serve.sh
+++ build_and_serve.sh
@@ -4,10 +4,12 @@
 echo "-- Building html doc..."
 
 if [[ "$1" == "--force" ]]; then
-    rm -rf docs/torchcnnbuilder/
+   rm -rf docs/*.html
 fi
 
 pdoc --html -o docs --config latex_math=True torchcnnbuilder/ --force
+mv docs/torchcnnbuilder/* docs/
+rm -rf docs/torchcnnbuilder
 
 # serving doc
 echo "--- Serving docs..."
